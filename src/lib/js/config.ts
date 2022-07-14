ff887f489bdf8ffe2dea29389523fe18d66ff9c7
DenebTM <deneb@screee.ee>
2022-07-14 23:38:18 +0200

    Me when git commit -a does not commit all files

---
+++ src/lib/js/config.ts
@@ -0,0 +1,17 @@
+export const filesBase = '/files'
+export const artBase = '/art'
+
+export const navItems = {
+    'Home': '/',
+    // 'Bark at me': '/social',
+    'Art gallery': '/art',
+    'Nextcloud': 'https://cloud.netdeneb.com/',
+    'Screemail': 'https://mail.screee.ee/',
+    'Other': {
+        // 'Public files': '/files',
+        'Best website (mirror)': '/yiff.toys',
+        // '天安门 Yes': '/pooh',
+        'Source code': 'https://github.com/DenebTM/netdeneb',
+        'More coming soon!': undefined
+    }
+}
