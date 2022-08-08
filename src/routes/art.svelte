e1d9e1162b50757ce935f249ff9cdc40b6fb8bd4
DenebTM <deneb@screee.ee>
2022-08-08 02:52:55 +0200

    Why did I put that there

--- src/routes/art.svelte
+++ src/routes/art.svelte
@@ -1,14 +1,6 @@
-<script lang="ts" context="module">
-export const load: Load = async ({ error, props, url }) => {
-    console.log(url)
-    console.log(error)
-    return { status: 200, props }
-}
-</script>
 <script lang="ts">
 import { setTitle } from '$lib/js/tools'
 import Gallery from '$lib/components/Gallery.svelte'
-import type { Load } from '@sveltejs/kit';
 
 export let imgList: ArtList
 
