334796eb701d8936e33900b88a3c9d61eed95200
DenebTM <deneb@screee.ee>
2023-06-26 21:38:58 +0200

    add src/lib back to config

--- src/util/styles.ts
+++ src/util/styles.ts
@@ -1,4 +1,3 @@
 import '~/assets/css/theme.css'
 import '~/assets/css/styles.css'
-import 'flag-icons/css/flag-icons.min.css'
 import 'boxicons/css/boxicons.min.css'
--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src'),
+      '$lib': path.resolve(__dirname, './src/lib'),
     },
   },
 })
