3486259c2f266983f0ca18d4607e9f1398501205
DenebTM <deneb@screee.ee>
2023-11-10 21:22:49 +0100

    URI-encode thumbnail redirect URL, copy GIFs instead of generating thumbnails, improve error handling

--- src/routes/thumbs/[...path]/+server.ts
+++ src/routes/thumbs/[...path]/+server.ts
@@ -1,24 +1,44 @@
 import { error, redirect } from '@sveltejs/kit'
 import type { RequestHandler } from '@sveltejs/kit'
-import { join as pathJoin, dirname } from 'node:path'
+import { join as pathJoin, dirname, extname } from 'node:path'
 import { promises as fs, existsSync as exists } from 'node:fs'
 import gm from 'gm'
 import { getConfig } from '~/util/appConfig'
+
+const validExtensions = [
+  '.jpg',
+  '.jpeg',
+  '.png',
+  '.gif',
+  '.webp',
+  '.tiff',
+  '.bmp',
+  '.heif',
+  '.heic',
+  '.svg',
+]
 
 export const GET: RequestHandler = async ({ params: { path } }) => {
   if (!path) {
     throw error(400, 'Missing file name')
   }
 
+  if (!validExtensions.includes(extname(path))) {
+    throw error(400, 'Not an image file')
+  }
+
   const { thumbsBasePath, thumbsBaseURL, filesBasePath } = await getConfig()
 
   const imgFileName = pathJoin(filesBasePath, path)
-  const thumbFileName = `${pathJoin(thumbsBasePath, path)}.webp`
-  const thumbURL = `${pathJoin(thumbsBaseURL, path)}.webp`
-
   if (!exists(imgFileName)) {
-    throw error(404, 'Image does not exist')
+    throw error(400, 'Source file does not exist')
   }
+
+  // copy GIFs instead of generating a thumbnail
+  const thumbFileName = `${pathJoin(thumbsBasePath, path)}${
+    extname(imgFileName) !== '.gif' ? '.webp' : ''
+  }`
+  const thumbURL = encodeURI(`${pathJoin(thumbsBaseURL, path)}.webp`)
 
   // create path to new thumbnail if it does not exist
   const thumbDirName = dirname(thumbFileName)
@@ -29,21 +49,25 @@ export const GET: RequestHandler = async ({ params: { path } }) => {
   // generate new thumbnail if one does not already exist
   if (!exists(thumbFileName)) {
     try {
-      await new Promise((resolve, reject) => {
-        console.log(`generating thumbnail for ${imgFileName}`)
-        gm(imgFileName)
-          .resize(512, 512, '^')
-          .gravity('Center')
-          .extent(512, 512)
-          .write(thumbFileName, error => {
-            if (error != null) {
-              console.error(error)
-              reject(error)
-            }
+      if (extname(imgFileName) === '.gif') {
+        await fs.copyFile(imgFileName, thumbFileName)
+      } else {
+        await new Promise((resolve, reject) => {
+          console.log(`generating thumbnail for ${imgFileName}`)
+          gm(imgFileName)
+            .resize(512, 512, '^')
+            .gravity('Center')
+            .extent(512, 512)
+            .write(thumbFileName, error => {
+              if (error != null) {
+                console.error(error)
+                reject(error)
+              }
 
-            resolve(thumbFileName)
-          })
-      })
+              resolve(thumbFileName)
+            })
+        })
+      }
     } catch (err) {
       throw error(500, String(err))
     }
