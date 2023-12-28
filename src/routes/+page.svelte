3e850d697281aec4a2cab9f6d1de958340ecab7a
DenebTM <deneb@screee.ee>
2023-12-28 02:54:12 +0100

    fix some html semantics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,29 +10,29 @@
   <title>{sitename}</title>
 </svelte:head>
 
-<div class="grid">
-  <section>
-    <img
-      id="me"
-      src="/img/deneb1.webp"
-      alt="Art of Deneb, by twitter.com/FluffySakora"
-    />
-    <h1>Hi!</h1>
-    <p>
-      I'm Deneb, a maned wolf and hobbyist developer from Austria, currently
-      studying comp-sci in Innsbruck.
-    </p>
-    <p>
-      This is mainly a place for me to put my personal pet projects, but you'll
-      also find my ref and other art I've received here. Still under
-      construction, so check back later for more!
-    </p>
-    {#if data.socials.length > 0}
-      <hr />
-      <Socials links={data.socials} />
-    {/if}
+<section id="blurb">
+  <img
+    id="me"
+    src="/img/deneb1.webp"
+    alt="Art of Deneb, by twitter.com/FluffySakora"
+  />
+  <h1>Hi!</h1>
+  <p>
+    I'm Deneb, a maned wolf and hobbyist developer from Austria, currently
+    studying comp-sci in Innsbruck.
+  </p>
+  <p>
+    This is mainly a place for me to put my personal pet projects, but you'll
+    also find my ref and other art I've received here. Still under construction,
+    so check back later for more!
+  </p>
+</section>
+{#if data.socials.length > 0}
+  <hr />
+  <section id="socials">
+    <Socials links={data.socials} />
   </section>
-</div>
+{/if}
 
 <style>
   #me {
