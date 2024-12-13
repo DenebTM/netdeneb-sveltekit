f1cc581f7e4a9c7fccfa1af1cf6ad7e86cb5b5c0
DenebTM <deneb@screee.ee>
2024-12-13 02:11:08 +0100

    plain friend list -> 88x31s

--- src/routes/(og-default)/links/+page.svelte
+++ src/routes/(og-default)/links/+page.svelte
@@ -14,7 +14,8 @@
     },
     {
       domain: 'kaleidosium.my.id',
-      button: 'https://sterophonick.github.io/assets/img/home/web_button.gif',
+      button: null,
+      // button: https://sterophonick.github.io/assets/img/home/88_dania.png
     },
     {
       domain: 'zenthefox.online',
@@ -88,117 +89,129 @@
     content="The mane's socials, friends, and other things" />
 </svelte:head>
 <div class="page-content">
-  <div class="page-column">
-    <h2>Yap at me!</h2>
-    <section id="email">
-      <p>
-        You may contact me via
-        <a rel="me" href="mailto:deneb@screee.ee">
-          <i class="inline-icon bx bx-sm bx-envelope" />&nbsp;<span>Email:</span
-          >&nbsp;deneb@screee.ee
-        </a><br />
-        Please direct any professional inquiries there, and there only.
-      </p>
-    </section>
+  <div class="column-box">
+    <div class="page-column">
+      <h2>Yap at me!</h2>
+      <section id="email">
+        <p>
+          You may contact me via
+          <a rel="me" href="mailto:deneb@screee.ee">
+            <i class="inline-icon bx bx-sm bx-envelope" />&nbsp;<span
+              >Email:</span
+            >&nbsp;deneb@screee.ee
+          </a><br />
+          Please direct any professional inquiries there, and there only.
+        </p>
+      </section>
 
-    <section id="socials">
-      <p>You can also find me on these platforms:</p>
-      <ul>
-        {#each data.socials as { platform, icon, href, text }}
+      <section id="socials">
+        <p>You can also find me on these platforms:</p>
+        <ul class="bulletless spaced">
+          {#each data.socials as { platform, icon, href, text }}
+            <li>
+              <a rel="me" {href}>
+                <i class={`inline-icon bx bx-sm ${icon}`} />&nbsp;<span
+                  >{platform}:</span>
+                {text}
+              </a>
+            </li>
+          {/each}
+        </ul>
+      </section>
+    </div>
+
+    <div class="page-column">
+      <section id="cool-stuff">
+        <h2>Things On The Web</h2>
+        <ul class="bulletless">
           <li>
-            <a rel="me" {href}>
-              <i class={`inline-icon bx bx-sm ${icon}`} />&nbsp;<span
-                >{platform}:</span>
-              {text}
-            </a>
+            <a href="https://it-syndikat.org/">IT-Syndikat</a>
+            — <WikiLink page="Hackerspace" /> in Innsbruck
           </li>
-        {/each}
-      </ul>
-    </section>
-  </div>
+          <li>
+            <a href="https://bahn.expert/">bahn.expert</a>
+            — real-time train info, DB and others
+          </li>
+          <li>
+            <a href="https://traewelling.de/">Träwelling</a>
+            — log and share your train journeys
+          </li>
+          <li>
+            <a href="https://travelynx.de/">Travelynx</a>
+            — log your train journeys more privately
+          </li>
+          <li>
+            <a href="https://cyberchef.com/">CyberChef</a>
+            — encode/decode most (text) formats
+          </li>
+          <li>
+            <a href="https://www.arcticpolygon.com/">Arctic Polygon</a>
+            — cute little webcomic with foxes
+          </li>
+          <li>
+            <a href="https://screee.ee/">screee.ee</a>
+            — fencs :&gt;
+          </li>
+          <li>
+            <a href="https://bigrat.monster/">bigrat.monster</a>
+            — yeah
+          </li>
+          <li>
+            <a href="https://slowroads.io/">slow roads</a>
+            — chill driving vibes
+          </li>
+          <li>
+            <a href="https://scp-wiki.wikidot.com/scp-2521">●●|●●●●●|●●|●</a>
+            — in case you somehow haven't seen it
+          </li>
+          <li>
+            <a href="https://computernewb.com/vncresolver/">VNC Resolver</a>
+            — secure your VNC server or end up here
+          </li>
+          <li>
+            <a href="https://old.reddit.com/r/opendirectories/"
+              >r/opendirectories</a>
+            — unsecured HTTP file servers
+          </li>
+          <li>
+            <a
+              href="https://eev.ee/blog/2012/04/09/php-a-fractal-of-bad-design/"
+              >PHP: a fractal of bad design</a>
+            — must-read blog post
+          </li>
+          <li>
+            <a href="https://freeplay.floof.company/">freeplay</a>
+            — fox on the internet, css wizard
+          </li>
+        </ul>
+      </section>
 
-  <div class="page-column">
-    <section id="cool-stuff">
-      <h2>Things On The Web</h2>
-      <ul>
-        <li>
-          <a href="https://it-syndikat.org/">IT-Syndikat</a>
-          — <WikiLink page="Hackerspace" /> in Innsbruck
-        </li>
-        <li>
-          <a href="https://bahn.expert/">bahn.expert</a>
-          — real-time train info, DB and others
-        </li>
-        <li>
-          <a href="https://traewelling.de/">Träwelling</a>
-          — log and share your train journeys
-        </li>
-        <li>
-          <a href="https://travelynx.de/">Travelynx</a>
-          — log your train journeys more privately
-        </li>
-        <li>
-          <a href="https://cyberchef.com/">CyberChef</a>
-          — encode/decode most (text) formats
-        </li>
-        <li>
-          <a href="https://www.arcticpolygon.com/">Arctic Polygon</a>
-          — cute little webcomic with foxes
-        </li>
-        <li>
-          <a href="https://screee.ee/">screee.ee</a>
-          — fencs :&gt;
-        </li>
-        <li>
-          <a href="https://bigrat.monster/">bigrat.monster</a>
-          — yeah
-        </li>
-        <li>
-          <a href="https://slowroads.io/">slow roads</a>
-          — chill driving vibes
-        </li>
-        <li>
-          <a href="https://scp-wiki.wikidot.com/scp-2521">●●|●●●●●|●●|●</a>
-          — in case you somehow haven't seen it
-        </li>
-        <li>
-          <a href="https://computernewb.com/vncresolver/">VNC Resolver</a>
-          — secure your VNC server or end up here
-        </li>
-        <li>
-          <a href="https://old.reddit.com/r/opendirectories/"
-            >r/opendirectories</a>
-          — unsecured HTTP file servers
-        </li>
-        <li>
-          <a href="https://eev.ee/blog/2012/04/09/php-a-fractal-of-bad-design/"
-            >PHP: a fractal of bad design</a>
-          — must-read blog post
-        </li>
-        <li>
-          <a href="https://freeplay.floof.company/">freeplay</a>
-          — fox on the internet, css wizard
-        </li>
-      </ul>
-    </section>
-
-    <section id="friends">
-      <h2>Friends' websites</h2>
-      <ul>
-        {#each friendlinks as { domain }}
-          <li><a href={`https://${domain}`}>{domain}</a></li>
-        {/each}
-      </ul>
-    </section>
-
-    <!-- <section id="artists">
+      <!-- <section id="artists">
       <h3>Furry artists you should look at</h3>
       <p>(see also <a href="/art">/art</a> :3)</p>
       <ul>
         <li><a href=""></a></li>
       </ul>
     </section> -->
+    </div>
   </div>
+
+  <section id="friends">
+    <h2>Friends' websites</h2>
+    <ul class="bulletless inline-list">
+      {#each friendlinks as { domain, button }}
+        <li>
+          <a class="button-link" href={`https://${domain}`}>
+            {#if button}
+              <img src={button} alt={domain} />
+            {:else}
+              <span>{domain}</span>
+            {/if}
+          </a>
+        </li>
+      {/each}
+    </ul>
+  </section>
 </div>
 
 <style>
@@ -211,14 +224,14 @@
   }
 
   @media not (max-width: 744px) {
-    .page-content {
+    .column-box {
       display: flex;
       flex-direction: row;
       flex-wrap: wrap;
     }
 
-    .page-content .page-column > h2,
-    .page-content section:first-child h2 {
+    .column-box .page-column > h2,
+    .column-box section:first-child h2 {
       margin-top: 0;
     }
   }
@@ -227,9 +240,19 @@
     padding-left: 1em;
   }
 
-  #socials li {
+  ul.bulletless li {
     list-style-type: none;
+  }
+
+  ul.spaced li {
     margin-bottom: 0.75em;
+  }
+
+  ul.inline-list {
+    padding-left: 0;
+  }
+  ul.inline-list li {
+    display: inline-block;
   }
 
   a[rel='me'] {
@@ -237,5 +260,21 @@
   }
   a[rel='me']:hover {
     font-weight: 600;
+  }
+
+  .button-link > :first-child {
+    display: inline-block;
+    width: 88px;
+    height: 31px;
+    margin: 0 0 4px 4px;
+    text-align: center;
+    overflow: hidden;
+  }
+  .button-link img {
+    image-rendering: pixelated;
+  }
+  .button-link span {
+    color: inherit;
+    border: 1px solid var(--primary);
   }
 </style>
