1e654bad8a0168ea852e5ae1294f8850d8b3c24a
DenebTM <deneb@screee.ee>
2024-12-13 03:58:04 +0100

    better logging

---
+++ src/hooks.server.ts
@@ -0,0 +1,14 @@
+import type { HandleServerError } from '@sveltejs/kit'
+
+export const handleError: HandleServerError = async ({
+  error,
+  event,
+  status,
+  message,
+}) => {
+  console.error(error, event, status, message)
+
+  const href_path = event.url.href.replace(event.url.origin, '')
+
+  console.error(`SvelteKit error: ${status} ${message} (path: '${href_path}')`)
+}
