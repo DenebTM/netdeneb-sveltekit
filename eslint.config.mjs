a25b4d18f41a4cbd1a4bff5f3d7f4252af658e6d
DenebTM <deneb@screee.ee>
2024-12-15 15:49:35 +0100

    fix npm lint script; format eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,56 +1,62 @@
-import svelte3 from "eslint-plugin-svelte3";
-import prettier from "eslint-plugin-prettier";
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import svelte3 from 'eslint-plugin-svelte3'
+import prettier from 'eslint-plugin-prettier'
+import globals from 'globals'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
 
-export default [...compat.extends("standard-with-typescript"), {
+export default [
+  ...compat.extends('standard-with-typescript'),
+  {
     plugins: {
-        svelte3,
-        prettier,
+      svelte3,
+      prettier,
     },
 
     languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
+      globals: {
+        ...globals.browser,
+      },
 
-        ecmaVersion: "latest",
-        sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
 
-        parserOptions: {
-            project: "tsconfig.json",
-        },
+      parserOptions: {
+        project: 'tsconfig.json',
+      },
     },
 
     rules: {
-        eqeqeq: "warn",
-        "no-empty": "warn",
-        "prefer-const": "warn",
-        "comma-dangle": "off",
-        "quote-props": "off",
+      eqeqeq: 'warn',
+      'no-empty': 'warn',
+      'prefer-const': 'warn',
+      'comma-dangle': 'off',
+      'quote-props': 'off',
 
-        "@typescript-eslint/comma-dangle": ["error", {
-            arrays: "always-multiline",
-            objects: "always-multiline",
-            imports: "never",
-            exports: "never",
-            functions: "never",
-        }],
+      '@typescript-eslint/comma-dangle': [
+        'error',
+        {
+          arrays: 'always-multiline',
+          objects: 'always-multiline',
+          imports: 'never',
+          exports: 'never',
+          functions: 'never',
+        },
+      ],
 
-        "@typescript-eslint/no-throw-literal": "off",
-        "@typescript-eslint/strict-boolean-expressions": "off",
-        "@typescript-eslint/no-extra-parens": "error",
-        "@typescript-eslint/space-before-function-paren": "off",
+      '@typescript-eslint/no-throw-literal': 'off',
+      '@typescript-eslint/strict-boolean-expressions': 'off',
+      '@typescript-eslint/no-extra-parens': 'error',
+      '@typescript-eslint/space-before-function-paren': 'off',
     },
-}];
+  },
+]
